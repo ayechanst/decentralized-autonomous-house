f98814c52eaf028b162d1a19f604d2f1b062f445
Aye Chan <ayechan.sant@gmail.com>
2023-08-16 10:12:15 -0700

    making group creation form

--- packages/nextjs/components/CreateGroup.tsx
+++ packages/nextjs/components/CreateGroup.tsx
@@ -1,15 +1,46 @@
 import React, { useState } from "react";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+
 
 export const CreateGroup = () => {
     const [createGroup, setCreateGroup] = useState(false);
+    const [groupName, setGroupName] = useState('');
+
+  const { writeAsync } = useScaffoldContractWrite({
+    contractName: "YourContract",
+    functionName: "createGroup",
+    args: [groupName],
+    onBlockConfirmation: txnReceipt => {
+      console.log("group created", txnReceipt.blockHash);
+    },
+  });
 
     function handleClick() {
        setCreateGroup(!createGroup);
     }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    writeAsync({ args: [groupName] });
+      console.log('group made')
+  }
+
+
     return (
         <>
             <button onClick={handleClick}>Create Group</button>
-            {createGroup && <div>Create group form goes here</div>}
+            {createGroup && <div>
+                          <form onSubmit={handleSubmit}>
+            <div className="py-1">
+              <input
+                placeholder="Purchase Name"
+                className="input input-bordered w-full max-w-xs"
+                value={groupName}
+                onChange={e => setGroupName(e.target.value)}
+              />
+            </div>
+                          </form>
+            </div>}
         </>
     )
 }
