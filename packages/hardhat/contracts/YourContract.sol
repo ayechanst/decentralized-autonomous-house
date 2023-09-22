688d92617534b6649391a65dec9efcfd7eb4abf7
Chad Bro <ayechan.sant@gmail.com>
2023-09-22 16:50:18 -0700

    correct erc721

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 // Useful for debugging. Remove when deploying to a live network.
 import "hardhat/console.sol";
-import "NFT.sol"
+import "TaskNFT.sol"
 
 contract YourContract {
 
