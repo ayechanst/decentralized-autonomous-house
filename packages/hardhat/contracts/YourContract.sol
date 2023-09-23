6a5d151e01e2be04ac9735cd59dd9786a94c3436
Chad Bro <ayechan.sant@gmail.com>
2023-09-22 19:41:51 -0700

    task functions and voting being worked on

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,15 +3,57 @@ pragma solidity >=0.8.0 <0.9.0;
 
 // Useful for debugging. Remove when deploying to a live network.
 import "hardhat/console.sol";
-import "TaskNFT.sol"
 
 contract YourContract {
 
+	struct Task {
+		string taskName;
+		string taskDescription;
+		uint256 taskWeight;
+	}
+
+	uint256 numOfMembers = 0;
+	uint256 minVotes = numOfMembers / 2;
 	mapping(address => bool) public isMember;
+	Task[] taskVotingQue;
 
 	modifier onlyMember() {
 		require(isMember[msg.sender], "You are not a member of the DAO.");
 		_;
+	}
+
+	function addMember(address newMember) external {
+		require(!isMember[newMember], "Address is already a member of the DAO.");
+		isMember[msg.sender] = true;
+		numOfMembers++;
+	}
+
+	function suggestTask(
+		string memory taskName,
+		string memory taskDescription,
+		uint256 taskWeight
+	) public onlyMember {
+		require(taskWeight < 11, "Task cannot weigh more than 10 units.");
+		Task memory newTask = Task({
+				taskName: taskName,
+				taskDescription: taskDescription,
+				taskWeight: taskWeight
+			});
+		taskVotingQue.push(newTask);
+	}
+
+	// TODO: Members can only vote once, make it so they cant vote twice
+	function voteOnTask(uint256 vote) external onlyMember {
+		address[] alreadyVoted;
+		// TODO: fix 47 and 49, no push method on memory
+		alreadyVoted.push(msg.sender);
+		for (uint256 i = 0; i < alreadyVoted.length; i++) {
+			require(alreadyVoted[i] != msg.sender, "You already voted");
+		}
+		Task memory taskBeingVoted = taskVotingQue[taskVotingQue.length];
+		if (vote > minVotes) {
+			// call the mint function passing in votingTask
+		}
 	}
 
 }
--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -5,51 +5,17 @@ const contracts = {
       name: "localhost",
       contracts: {
         YourContract: {
-          address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
           abi: [
             {
-              anonymous: false,
               inputs: [
-                {
-                  indexed: false,
-                  internalType: "uint256",
-                  name: "upVote",
-                  type: "uint256",
-                },
-                {
-                  indexed: false,
-                  internalType: "uint256",
-                  name: "downVote",
-                  type: "uint256",
-                },
-              ],
-              name: "VoteCounts",
-              type: "event",
-            },
-            {
-              inputs: [
-                {
-                  internalType: "string",
-                  name: "taskName",
-                  type: "string",
-                },
-                {
-                  internalType: "string",
-                  name: "taskDescription",
-                  type: "string",
-                },
                 {
                   internalType: "address",
-                  name: "groupAddress",
+                  name: "newMember",
                   type: "address",
                 },
-                {
-                  internalType: "string[]",
-                  name: "taskParticipants",
-                  type: "string[]",
-                },
               ],
-              name: "createTask",
+              name: "addMember",
               outputs: [],
               stateMutability: "nonpayable",
               type: "function",
@@ -58,244 +24,19 @@ const contracts = {
               inputs: [
                 {
                   internalType: "address",
-                  name: "key",
+                  name: "",
                   type: "address",
                 },
               ],
-              name: "getTasks",
+              name: "isMember",
               outputs: [
                 {
-                  components: [
-                    {
-                      internalType: "string",
-                      name: "name",
-                      type: "string",
-                    },
-                    {
-                      internalType: "string",
-                      name: "description",
-                      type: "string",
-                    },
-                    {
-                      internalType: "uint256",
-                      name: "grade",
-                      type: "uint256",
-                    },
-                    {
-                      internalType: "enum YourContract.Status",
-                      name: "taskStatus",
-                      type: "uint8",
-                    },
-                    {
-                      internalType: "string[]",
-                      name: "taskParticipants",
-                      type: "string[]",
-                    },
-                    {
-                      internalType: "uint256",
-                      name: "personsTurn",
-                      type: "uint256",
-                    },
-                    {
-                      internalType: "uint256",
-                      name: "upVote",
-                      type: "uint256",
-                    },
-                    {
-                      internalType: "uint256",
-                      name: "downVote",
-                      type: "uint256",
-                    },
-                  ],
-                  internalType: "struct YourContract.Task[]",
+                  internalType: "bool",
                   name: "",
-                  type: "tuple[]",
+                  type: "bool",
                 },
               ],
               stateMutability: "view",
-              type: "function",
-            },
-            {
-              inputs: [
-                {
-                  internalType: "address",
-                  name: "",
-                  type: "address",
-                },
-                {
-                  internalType: "uint256",
-                  name: "",
-                  type: "uint256",
-                },
-              ],
-              name: "peopleMapping",
-              outputs: [
-                {
-                  internalType: "string",
-                  name: "name",
-                  type: "string",
-                },
-                {
-                  internalType: "address",
-                  name: "personAddress",
-                  type: "address",
-                },
-                {
-                  internalType: "uint256",
-                  name: "balance",
-                  type: "uint256",
-                },
-                {
-                  internalType: "uint256",
-                  name: "reputation",
-                  type: "uint256",
-                },
-              ],
-              stateMutability: "view",
-              type: "function",
-            },
-            {
-              inputs: [
-                {
-                  internalType: "address",
-                  name: "",
-                  type: "address",
-                },
-                {
-                  internalType: "uint256",
-                  name: "",
-                  type: "uint256",
-                },
-              ],
-              name: "taskMapping",
-              outputs: [
-                {
-                  internalType: "string",
-                  name: "name",
-                  type: "string",
-                },
-                {
-                  internalType: "string",
-                  name: "description",
-                  type: "string",
-                },
-                {
-                  internalType: "uint256",
-                  name: "grade",
-                  type: "uint256",
-                },
-                {
-                  internalType: "enum YourContract.Status",
-                  name: "taskStatus",
-                  type: "uint8",
-                },
-                {
-                  internalType: "uint256",
-                  name: "personsTurn",
-                  type: "uint256",
-                },
-                {
-                  internalType: "uint256",
-                  name: "upVote",
-                  type: "uint256",
-                },
-                {
-                  internalType: "uint256",
-                  name: "downVote",
-                  type: "uint256",
-                },
-              ],
-              stateMutability: "view",
-              type: "function",
-            },
-            {
-              inputs: [
-                {
-                  internalType: "uint256",
-                  name: "",
-                  type: "uint256",
-                },
-              ],
-              name: "taskVotingQue",
-              outputs: [
-                {
-                  internalType: "string",
-                  name: "name",
-                  type: "string",
-                },
-                {
-                  internalType: "string",
-                  name: "description",
-                  type: "string",
-                },
-                {
-                  internalType: "uint256",
-                  name: "grade",
-                  type: "uint256",
-                },
-                {
-                  internalType: "enum YourContract.Status",
-                  name: "taskStatus",
-                  type: "uint8",
-                },
-                {
-                  internalType: "uint256",
-                  name: "personsTurn",
-                  type: "uint256",
-                },
-                {
-                  internalType: "uint256",
-                  name: "upVote",
-                  type: "uint256",
-                },
-                {
-                  internalType: "uint256",
-                  name: "downVote",
-                  type: "uint256",
-                },
-              ],
-              stateMutability: "view",
-              type: "function",
-            },
-            {
-              inputs: [
-                {
-                  internalType: "address",
-                  name: "key",
-                  type: "address",
-                },
-                {
-                  internalType: "string",
-                  name: "taskName",
-                  type: "string",
-                },
-                {
-                  internalType: "uint256",
-                  name: "vote",
-                  type: "uint256",
-                },
-              ],
-              name: "vote",
-              outputs: [],
-              stateMutability: "nonpayable",
-              type: "function",
-            },
-            {
-              inputs: [
-                {
-                  internalType: "address",
-                  name: "voter",
-                  type: "address",
-                },
-                {
-                  internalType: "uint256",
-                  name: "vote",
-                  type: "uint256",
-                },
-              ],
-              name: "voteOnTask",
-              outputs: [],
-              stateMutability: "nonpayable",
               type: "function",
             },
           ],
